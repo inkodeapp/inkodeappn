af26c370253e28b0b0dc0dcbad069cdfaa3e5029
inkodeapp <53095543+inkodeapp@users.noreply.github.com>
2021-03-28 17:04:15 -0300

    add modulo cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const morgan = require("morgan");
 const router = require("./src/Routes/Route");
 
 app.set("port", process.env.PORT || 80);
 app.use(morgan("dev"));
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
